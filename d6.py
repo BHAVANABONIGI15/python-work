a6b1d974952127bafd9d4c911519ea9d490dfe68
BHAVANABONIGI15 <bonigibhavana@gmail.com>
2026-03-04 12:11:23 +0530

    Add files via upload

---
+++ d6.py
@@ -0,0 +1,12 @@
+n=int(input("enter number of students :"))
+students ={}
+
+for i in range(n):
+    name = input("enter name :")
+    cgpa = float(input("enter cgpa:"))
+    names.append(name)
+    cgpa.append(cgpa)
+
+for i in range (len(names)):
+    
+                    
